f0cf23125304c4ac389f0c383f2b188400935280
neil mehra <neilmehra@outlook.com>
2023-07-19 12:40:59 -0400

    can this commit work

--- app/db/session.py
+++ app/db/session.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 
 def create_postgres_url():
     if settings.DATABASE_URL:
-        return settings.DATABASE_URL
+        return settings.DATABASE_URL.replace("postgres", "postgresql")
     else:
         return f"postgresql://{settings.POSTGRES_USER}{':' + settings.POSTGRES_PASSWORD if settings.POSTGRES_PASSWORD else ''}@{settings.POSTGRES_IP}{':' + settings.POSTGRES_PORT if settings.POSTGRES_PORT else ''}/{settings.POSTGRES_DB}"
 
