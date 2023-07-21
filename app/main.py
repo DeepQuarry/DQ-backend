821905f61d1283b75b5edc0ac924f40080ac5657
neil mehra <neilmehra@outlook.com>
2023-07-20 22:51:55 -0400

    fix cors middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import main_router
@@ -8,27 +9,38 @@ from app.core.log import generate_logger
 logger = generate_logger()
 logger.info("Starting API")
 
-app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
+# origins = [
+#     "*"  # just for now
+#     # "https://deepquarry-pi.vercel.app",
+#     # "https://deepquarry.vercel.app" "http://localhost",
+#     # "http://localhost:8080",
+# ]
+#
+# regex_origin = ["https?//.*\.deepquarry.*vercel.app", "https?//.*\.localhost:?\d*"]
+
+# middleware = [
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     # allow_origin_regex=regex_origin,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST"],
+#     allow_headers=["*"],
+# ]
 
 
-origins = [
-    "*"  # just for now
-    # "https://deepquarry-pi.vercel.app",
-    # "https://deepquarry.vercel.app" "http://localhost",
-    # "http://localhost:8080",
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        # allow_origin_regex=regex_origin,
+        allow_credentials=True,
+        allow_methods=["GET", "POST"],
+        allow_headers=["*"],
+    )
 ]
 
-regex_origin = ["https?//.*\.deepquarry.*vercel.app", "https?//.*\.localhost:?\d*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    # allow_origin_regex=regex_origin,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json", middleware=middleware
 )
 
 
