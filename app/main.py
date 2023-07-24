7cd21d6deb342b40bb58b1b63f1cd6d5feae49a5
neil mehra <neilmehra@outlook.com>
2023-07-23 22:43:22 -0400

    manually add headers in middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
@@ -18,31 +18,31 @@ logger.info("Starting API")
 #
 # regex_origin = ["https?//.*\.deepquarry.*vercel.app", "https?//.*\.localhost:?\d*"]
 
-# middleware = [
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     # allow_origin_regex=regex_origin,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST"],
-#     allow_headers=["*"],
-# ]
-
-
-middleware = [
-    Middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        # # allow_origin_regex=regex_origin,
-        # allow_credentials=True,
-        # allow_methods=["GET", "POST"],
-        # allow_headers=["*"],
-    )
-]
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json", middleware=middleware
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+# Salt to your taste
+ALLOWED_ORIGINS = '*'    # or 'foo.com', etc.
+
+# handle CORS preflight requests
+@app.options('/{rest_of_path:path}')
+async def preflight_handler(request: Request, rest_of_path: str) -> Response:
+    response = Response()
+    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+    return response
+
+# set CORS headers
+@app.middleware("http")
+async def add_CORS_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+    return response
 
 app.include_router(main_router, prefix=settings.API_V1_STR)
 
